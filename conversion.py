ee091fa9861eede45bef86b6179043d7a4184814
Nicolas Gruel <nicolas.gruel@gmail.com>
2018-03-09 15:42:39 +0000

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -6,8 +6,7 @@ def gallons2liters(gallons):
     liters = gallons * 4.54
     return liters
 
-# Convert the number of moles to 
-atoms
+# Convert the number of moles to atoms
 def moles2atoms(moles):
     atoms = moles * 6.023e23
     return atoms
